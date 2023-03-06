d972c0b1b0689bd0ed663dcc659c345ee7075aec
amelie-lr <112986063+amelie-lr@users.noreply.github.com>
2023-03-06 12:18:26 -0500

    styles et scripts pour ajustement nbre col

--- scripts.js
+++ scripts.js
@@ -15,3 +15,47 @@ function closeDetails() {
          x[i].removeAttribute("open");
 }
 }
+
+
+
+// source : https://www.w3schools.com/css/tryit.asp?filename=trycss3_var_js
+
+// Get the root element
+var r = document.querySelector(':root');
+
+// Create a function for setting a variable value
+function col1() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--doublewidth', '100% - (2 * 0.75%');
+    r.style.setProperty('--col', '1');
+    r.style.setProperty('--ratio', '0.5');
+    r.style.setProperty('--base', '3vw');
+}
+function col2() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--doublewidth', '2 * (var(--divwidth) + 0.75%)'); // besoin de recopier ici pour que le calcul se refasse correctement
+    r.style.setProperty('--col', '2');
+    r.style.setProperty('--ratio', '2.0309');
+    r.style.setProperty('--base', '1.5vw');
+}
+function col3() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--doublewidth', '2 * (var(--divwidth) + 0.75%)'); // besoin de recopier ici pour que le calcul se refasse correctement
+    r.style.setProperty('--col', '3');
+    r.style.setProperty('--ratio', '2.0471');
+    r.style.setProperty('--base', '1vw');
+  }
+function col4() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--doublewidth', '2 * (var(--divwidth) + 0.75%)'); // besoin de recopier ici pour que le calcul se refasse correctement
+    r.style.setProperty('--col', '4');
+    r.style.setProperty('--ratio', '2.0638');
+    r.style.setProperty('--base', '0.75vw');
+}
+function col5() {
+    // Set the value of variable --blue to another value (in this case "lightblue")
+    r.style.setProperty('--doublewidth', '2 * (var(--divwidth) + 0.75%)'); // besoin de recopier ici pour que le calcul se refasse correctement
+    r.style.setProperty('--col', '5');
+    r.style.setProperty('--ratio', '2.0811');
+    r.style.setProperty('--base', '0.6vw');
+}
